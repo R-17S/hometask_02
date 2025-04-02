14ac11fe1181795d6156089ed82ca7a3b123a0af
Vlad Lapusta <vlad.lapusta@bk.ru>
2025-04-02 18:23:46 +0300

    fix gitignore / fix testing endpoint route address

--- src/routes/testing/app.ts
+++ src/routes/testing/app.ts
@@ -4,7 +4,7 @@ import {Router, Request, Response} from "express";
 
 export const testingRouter = Router();
 
-testingRouter.delete('/allData', (req: Request, res: Response) => {
+testingRouter.delete('/all-data', (req: Request, res: Response) => {
     setDB()
     res.status(204).end()
 });
