3ccee9c2fce71e6adb19effacf50dcf8973904e7
Vlad Lapusta <vlad.lapusta@bk.ru>
2025-05-11 13:30:16 +0300

    fix 1

--- src/routes/testing/app.ts
+++ src/routes/testing/app.ts
@@ -1,10 +1,15 @@
 import {setDB} from "../../db/db";
 import {Router, Request, Response} from "express";
+import {blogsCollection, postsCollection} from "../../db/mongoDB";
 
 
 export const testingRouter = Router();
 
-testingRouter.delete('/all-data', (req: Request, res: Response) => {
-    setDB()
+testingRouter.delete('/all-data', async (req: Request, res: Response) => {
+    // setDB()
+    await  Promise.all([
+        postsCollection.deleteMany({}),
+        blogsCollection.deleteMany({}),
+    ])
     res.status(204).end()
 });
